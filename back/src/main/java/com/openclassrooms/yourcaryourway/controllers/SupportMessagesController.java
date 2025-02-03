8ae73f7d131158f4215a24c86ef10e77616cde41
Torus-Ortie <velez.romain@orange.fr>
2025-02-03 09:52:51 +0100

    Rework data name

--- back/src/main/java/com/openclassrooms/yourcaryourway/controllers/SupportMessagesController.java
+++ back/src/main/java/com/openclassrooms/yourcaryourway/controllers/SupportMessagesController.java
@@ -1,11 +1,9 @@
 package com.openclassrooms.yourcaryourway.controllers;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.util.HtmlUtils;
 
 import com.openclassrooms.yourcaryourway.models.SupportMessages;
 import com.openclassrooms.yourcaryourway.services.SupportMessagesService;
@@ -19,6 +17,6 @@ public class SupportMessagesController {
 
     @MessageMapping("/message") // Quand un client envoie un message à /api/message
     public void sendMessage(SupportMessages supportmessages) {
-        supportmessagesservice.sendSupportMessage(supportmessages.getReservationId(), supportmessages);
+        supportmessagesservice.sendMessageToUser(supportmessages.getReservationId(), supportmessages);
     }
 }
--- back/src/main/java/com/openclassrooms/yourcaryourway/models/SupportMessages.java
+++ back/src/main/java/com/openclassrooms/yourcaryourway/models/SupportMessages.java
@@ -5,24 +5,31 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "supportmessages")
 public class SupportMessages {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "messageid", nullable = false)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "userid", nullable = false)
     private String userId;
 
-    @Column(nullable = false)
+    @Column(name = "reservationid", nullable = false)
     private String reservationId;
 
     @Column(nullable = false)
     private String content;
 
     @CreationTimestamp
-    private LocalDateTime createdDate;
+    private LocalDateTime createdat;
 }
